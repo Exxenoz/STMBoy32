d9cc77f291d082b249481f78c356dda3129d5143
Exxenoz <exxenoz@gmx.at>
2017-09-11 21:05:38 +0200

    Increased clock frequency by changing PLL_M from 25 to 8 and added clock debug function for debugging

--- Project/Source/main.c
+++ Project/Source/main.c
@@ -12,6 +12,19 @@
 #include "lcd.h"
 #include "input.h"
 
+void ClockDebug_Initialize()
+{
+    RCC->CFGR |= RCC_CFGR_MCO2PRE_2;
+
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitObject;
+    GPIO_InitObject.GPIO_Mode  = GPIO_Mode_AF;
+    GPIO_InitObject.GPIO_Pin   = GPIO_Pin_9;
+    GPIO_InitObject.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOC, &GPIO_InitObject);
+}
+
 int main(void)
 {
     /*!< At this stage the microcontroller clock setting is already configured, 
@@ -20,6 +33,10 @@ int main(void)
        To reconfigure the default setting of SystemInit() function, 
        refer to system_stm32f4xx.c file */
     
+    // Uncomment the following line if you want
+    // to debug clock frequency settings using GPIO
+    //ClockDebug_Initialize();
+
     Input_Initialize();
 
     LCD_Initialize();
