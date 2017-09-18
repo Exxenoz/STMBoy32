c8545a7605739561251ee4ec2fb91bec5e7eb75f
Exxenoz <exxenoz@gmx.at>
2017-09-18 15:27:15 +0200

    Added methods for RGB LED disabling/enabling

--- Project/LED/led.c
+++ Project/LED/led.c
@@ -6,12 +6,12 @@ void LED_Initialize(void)
     RCC_APB1PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
 
     GPIO_InitTypeDef GPIO_InitObject;
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitObject;
-    TIM_OCInitTypeDef TIM_OCInitObject;
 
     #define INITIALIZE_OUTPUT_PIN(PORT, PIN)        \
-    GPIO_InitObject.GPIO_Mode  = GPIO_Mode_AF;      \
+    GPIO_InitObject.GPIO_Mode  = GPIO_Mode_OUT;     \
+    GPIO_InitObject.GPIO_OType = GPIO_OType_OD;     \
     GPIO_InitObject.GPIO_Pin   = PIN;               \
+    GPIO_InitObject.GPIO_PuPd  = GPIO_PuPd_NOPULL;  \
     GPIO_InitObject.GPIO_Speed = GPIO_Speed_100MHz; \
     GPIO_Init(PORT, &GPIO_InitObject);              \
 
@@ -19,39 +19,49 @@ void LED_Initialize(void)
     INITIALIZE_OUTPUT_PIN(LED_GREEN_PORT, LED_GREEN_PIN);
     INITIALIZE_OUTPUT_PIN(LED_BLUE_PORT, LED_BLUE_PIN);
 
-    GPIO_PinAFConfig(LED_RED_PORT,   LED_RED_PIN_SOURCE,   GPIO_AF_TIM1);
-    GPIO_PinAFConfig(LED_GREEN_PORT, LED_GREEN_PIN_SOURCE, GPIO_AF_TIM1);
-    GPIO_PinAFConfig(LED_BLUE_PORT,  LED_BLUE_PIN_SOURCE,  GPIO_AF_TIM1);
+    LED_EnableRed(false);
+    LED_EnableGreen(false);
+    LED_EnableBlue(true);
+}
 
-    uint32_t frequency = 1000000; // 1kHz
-    uint32_t period    = frequency / 1000;
+void LED_EnableRed(bool enable)
+{
+    if (enable)
+    {
+        LED_RED_RESET;
+        LED_GREEN_SET;
+        LED_BLUE_SET;
+    }
+    else
+    {
+        LED_RED_SET;
+    }
+}
 
-    TIM_TimeBaseInitObject.TIM_Prescaler = (uint16_t)((SystemCoreClock / frequency) - 1);
-    TIM_TimeBaseInitObject.TIM_Period = period;
-    TIM_TimeBaseInitObject.TIM_ClockDivision = 0;
-    TIM_TimeBaseInitObject.TIM_CounterMode = TIM_CounterMode_Up;
-    TIM_TimeBaseInitObject.TIM_RepetitionCounter = 0;
-    TIM_TimeBaseInit(TIM1, &TIM_TimeBaseInitObject);
+void LED_EnableGreen(bool enable)
+{
+    if (enable)
+    {
+        LED_RED_SET;
+        LED_GREEN_RESET;
+        LED_BLUE_SET;
+    }
+    else
+    {
+        LED_GREEN_SET;
+    }
+}
 
-    TIM_OCInitObject.TIM_OCMode = TIM_OCMode_PWM1;
-    TIM_OCInitObject.TIM_OCPolarity = TIM_OCPolarity_High;
-
-    TIM_OCInitObject.TIM_OutputState = TIM_OutputState_Enable;
-    TIM_OCInitObject.TIM_Pulse = period;
-
-    TIM_OC1Init(TIM1, &TIM_OCInitObject);
-    TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Enable);
-
-    TIM_OC2Init(TIM1, &TIM_OCInitObject);
-    TIM_OC2PreloadConfig(TIM1, TIM_OCPreload_Enable);
-
-    TIM_OC3Init(TIM1, &TIM_OCInitObject);
-    TIM_OC3PreloadConfig(TIM1, TIM_OCPreload_Enable);
-
-    TIM_OC4Init(TIM1, &TIM_OCInitObject);
-    TIM_OC4PreloadConfig(TIM1, TIM_OCPreload_Enable);
-
-    TIM_ARRPreloadConfig(TIM1, ENABLE);
-
-    TIM_Cmd(TIM1, ENABLE);
+void LED_EnableBlue(bool enable)
+{
+    if (enable)
+    {
+        LED_RED_SET;
+        LED_GREEN_SET;
+        LED_BLUE_RESET;
+    }
+    else
+    {
+        LED_BLUE_SET;
+    }
 }
--- Project/LED/led.h
+++ Project/LED/led.h
@@ -4,6 +4,19 @@
 #include "common.h"
 #include "led_config.h"
 
+#define LED_RED_SET     LED_RED_PORT->BSRRL |= LED_RED_PIN
+#define LED_RED_RESET   LED_RED_PORT->BSRRH |= LED_RED_PIN
+
+#define LED_GREEN_SET   LED_GREEN_PORT->BSRRL |= LED_GREEN_PIN
+#define LED_GREEN_RESET LED_GREEN_PORT->BSRRH |= LED_GREEN_PIN
+
+#define LED_BLUE_SET    LED_BLUE_PORT->BSRRL |= LED_BLUE_PIN
+#define LED_BLUE_RESET  LED_BLUE_PORT->BSRRH |= LED_BLUE_PIN
+
 void LED_Initialize(void);
+
+void LED_EnableRed(bool enable);
+void LED_EnableGreen(bool enable);
+void LED_EnableBlue(bool enable);
 
 #endif //LED_H
