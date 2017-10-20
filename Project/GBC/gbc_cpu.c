bae82a62edf3ba7ef84fedbcfea3ba159238d50c
Andreas Krischke <exxenoz@gmx.at>
2017-10-20 16:11:25 +0200

    Improved GBC CPU instruction handler for opcode 0x7 and implemented instruction 0x9

--- Project/GBC/gbc_cpu.c
+++ Project/GBC/gbc_cpu.c
@@ -8,24 +8,26 @@ uint8_t GBC_CPU_IncrementByte(uint8_t value)
 {
     value++;
 
+    // Carry flag not affected
+
     if ((value & 0xF) == 0)
     {
-        GBC_CPU_Register.F |= GBC_CPU_FLAGS_HALFCARRY;
+        GBC_CPU_FLAGS_SET(GBC_CPU_FLAGS_HALFCARRY);
     }
     else
     {
-        GBC_CPU_Register.F &= ~GBC_CPU_FLAGS_HALFCARRY;
+        GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_HALFCARRY);
     }
 
-    GBC_CPU_Register.F &= ~GBC_CPU_FLAGS_SUBTRACTION;
+    GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_SUBTRACTION);
 
     if (value)
     {
-        GBC_CPU_Register.F &= ~GBC_CPU_FLAGS_ZERO;
+        GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_ZERO);
     }
     else
     {
-        GBC_CPU_Register.F |= GBC_CPU_FLAGS_ZERO;
+        GBC_CPU_FLAGS_SET(GBC_CPU_FLAGS_ZERO);
     }
 
     return value;
@@ -35,27 +37,60 @@ uint8_t GBC_CPU_DecrementByte(uint8_t value)
 {
     value--;
 
+    // Carry flag not affected
+
     if ((value & 0xF) == 0xF)
     {
-        GBC_CPU_Register.F |= GBC_CPU_FLAGS_HALFCARRY;
+        GBC_CPU_FLAGS_SET(GBC_CPU_FLAGS_HALFCARRY);
     }
     else
     {
-        GBC_CPU_Register.F &= ~GBC_CPU_FLAGS_HALFCARRY;
+        GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_HALFCARRY);
     }
 
-    GBC_CPU_Register.F |= GBC_CPU_FLAGS_SUBTRACTION;
+    GBC_CPU_FLAGS_SET(GBC_CPU_FLAGS_SUBTRACTION);
 
     if (value)
     {
-        GBC_CPU_Register.F &= ~GBC_CPU_FLAGS_ZERO;
+        GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_ZERO);
     }
     else
     {
-        GBC_CPU_Register.F |= GBC_CPU_FLAGS_ZERO;
+        GBC_CPU_FLAGS_SET(GBC_CPU_FLAGS_ZERO);
     }
 
     return value;
+}
+
+uint16_t GBC_CPU_AddShorts(uint16_t a, uint16_t b)
+{
+    uint32_t result = a + b;
+    
+    if (result & 0x10000)
+    {
+        GBC_CPU_FLAGS_SET(GBC_CPU_FLAGS_CARRY);
+    }
+    else
+    {
+        GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_CARRY);
+    }
+
+    result &= 0xFFFF;
+
+    if (((a & 0xFFF) + (b & 0xFFF)) & 0x1000)
+    {
+        GBC_CPU_FLAGS_SET(GBC_CPU_FLAGS_HALFCARRY);
+    }
+    else
+    {
+        GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_HALFCARRY);
+    }
+    
+    GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_SUBTRACTION);
+
+    // Zero flag not affected
+
+    return result;
 }
 
 void GBC_CPU_NOP()                      // 0x00 - No operation
@@ -95,8 +130,6 @@ void GBC_CPU_LD_B_X(uint8_t operand)    // 0x06 - Load 8-bit immediate into B
 
 void GBC_CPU_RLC_A()                    // 0x07 - Rotate A left with carry
 {
-    GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_ALL);
-
     uint8_t value = GBC_CPU_Register.A;
 
     if (value & 0x8)
@@ -109,11 +142,23 @@ void GBC_CPU_RLC_A()                    // 0x07 - Rotate A left with carry
     else
     {
         value <<= 1;
+
+        GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_CARRY);
+    }
+
+    GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_HALFCARRY);
+    GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_SUBTRACTION);
+
+    if (value)
+    {
+        GBC_CPU_FLAGS_CLEAR(GBC_CPU_FLAGS_ZERO);
+    }
+    else
+    {
+        GBC_CPU_FLAGS_SET(GBC_CPU_FLAGS_ZERO);
     }
 
     GBC_CPU_Register.A = value;
-
-    // ToDo: Zero flag check?
 }
 
 void GBC_CPU_LD_XX_SP(uint16_t operand) // 0x08 - Save SP to given address
@@ -121,18 +166,24 @@ void GBC_CPU_LD_XX_SP(uint16_t operand) // 0x08 - Save SP to given address
     GBC_MMU_WriteShort(operand, GBC_CPU_Register.SP);
 }
 
+void GBC_CPU_ADD_HL_BC()                // 0x09 - Add 16-bit BC to HL
+{
+    GBC_CPU_Register.HL = GBC_CPU_AddShorts(GBC_CPU_Register.HL, GBC_CPU_Register.BC);
+}
+
 /*******************************************************************************/
 /* Opcode table and comments from http://imrannazar.com/Gameboy-Z80-Opcode-Map */
 /*******************************************************************************/
-const GBC_CPU_Instruction_t GBC_CPU_Instructions[9] =
+const GBC_CPU_Instruction_t GBC_CPU_Instructions[10] =
 {
-    { GBC_CPU_NOP,      GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_2  }, // 0x00 - No operation
-    { GBC_CPU_LD_BC_XX, GBC_CPU_OPERAND_BYTES_2, GBC_CPU_TICKS_6  }, // 0x01 - Load 16-bit immediate into BC
-    { GBC_CPU_LD_BCP_A, GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_4  }, // 0x02 - Save A to address pointed by BC
-    { GBC_CPU_INC_BC,   GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_4  }, // 0x03 - Increment 16-bit BC
-    { GBC_CPU_INC_B,    GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_2  }, // 0x04 - Increment B
-    { GBC_CPU_DEC_B,    GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_2  }, // 0x05 - Decrement B
-    { GBC_CPU_LD_B_X,   GBC_CPU_OPERAND_BYTES_1, GBC_CPU_TICKS_4  }, // 0x06 - Load 8-bit immediate into B
-    { GBC_CPU_RLC_A,    GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_4  }, // 0x07 - Rotate A left with carry
-    { GBC_CPU_LD_XX_SP, GBC_CPU_OPERAND_BYTES_2, GBC_CPU_TICKS_10 }, // 0x08 - Save SP to given address
+    { GBC_CPU_NOP,       GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_2  }, // 0x00 - No operation
+    { GBC_CPU_LD_BC_XX,  GBC_CPU_OPERAND_BYTES_2, GBC_CPU_TICKS_6  }, // 0x01 - Load 16-bit immediate into BC
+    { GBC_CPU_LD_BCP_A,  GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_4  }, // 0x02 - Save A to address pointed by BC
+    { GBC_CPU_INC_BC,    GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_4  }, // 0x03 - Increment 16-bit BC
+    { GBC_CPU_INC_B,     GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_2  }, // 0x04 - Increment B
+    { GBC_CPU_DEC_B,     GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_2  }, // 0x05 - Decrement B
+    { GBC_CPU_LD_B_X,    GBC_CPU_OPERAND_BYTES_1, GBC_CPU_TICKS_4  }, // 0x06 - Load 8-bit immediate into B
+    { GBC_CPU_RLC_A,     GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_4  }, // 0x07 - Rotate A left with carry
+    { GBC_CPU_LD_XX_SP,  GBC_CPU_OPERAND_BYTES_2, GBC_CPU_TICKS_10 }, // 0x08 - Save SP to given address
+    { GBC_CPU_ADD_HL_BC, GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_4  }, // 0x09 - Add 16-bit BC to HL
 };
