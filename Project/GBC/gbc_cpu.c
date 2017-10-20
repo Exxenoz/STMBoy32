bfd2c90a4f8f629013fd183587611e676bbee528
Andreas Krischke <exxenoz@gmx.at>
2017-10-20 14:33:09 +0200

    Implemented GBC CPU instruction 0x05

--- Project/GBC/gbc_cpu.c
+++ Project/GBC/gbc_cpu.c
@@ -18,7 +18,34 @@ uint8_t GBC_CPU_IncrementByte(uint8_t value)
     }
 
     GBC_CPU_Register.F &= ~GBC_CPU_FLAGS_SUBTRACTION;
-    
+
+    if (value)
+    {
+        GBC_CPU_Register.F &= ~GBC_CPU_FLAGS_ZERO;
+    }
+    else
+    {
+        GBC_CPU_Register.F |= GBC_CPU_FLAGS_ZERO;
+    }
+
+    return value;
+}
+
+uint8_t GBC_CPU_DecrementByte(uint8_t value)
+{
+    value--;
+
+    if ((value & 0xF) == 0xF)
+    {
+        GBC_CPU_Register.F |= GBC_CPU_FLAGS_HALFCARRY;
+    }
+    else
+    {
+        GBC_CPU_Register.F &= ~GBC_CPU_FLAGS_HALFCARRY;
+    }
+
+    GBC_CPU_Register.F |= GBC_CPU_FLAGS_SUBTRACTION;
+
     if (value)
     {
         GBC_CPU_Register.F &= ~GBC_CPU_FLAGS_ZERO;
@@ -56,11 +83,17 @@ void GBC_CPU_INC_B()                    // 0x04 - Increment register B
     GBC_CPU_Register.B = GBC_CPU_IncrementByte(GBC_CPU_Register.B);
 }
 
-const GBC_CPU_Instruction_t GBC_CPU_Instructions[5] =
+void GBC_CPU_DEC_B()                    // 0x05 - Decrement register B
+{
+    GBC_CPU_Register.B = GBC_CPU_DecrementByte(GBC_CPU_Register.B);
+}
+
+const GBC_CPU_Instruction_t GBC_CPU_Instructions[6] =
 {
     { GBC_CPU_NOP,      GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_2 }, // 0x00 - No operation
     { GBC_CPU_LD_BC_XX, GBC_CPU_OPERAND_BYTES_2, GBC_CPU_TICKS_6 }, // 0x01 - Load 2 bytes into BC
     { GBC_CPU_LD_BCP_A, GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_4 }, // 0x02 - Save A to address pointed by BC
     { GBC_CPU_INC_BC,   GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_4 }, // 0x03 - Increment register BC
     { GBC_CPU_INC_B,    GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_2 }, // 0x04 - Increment register B
+    { GBC_CPU_DEC_B,    GBC_CPU_OPERAND_BYTES_0, GBC_CPU_TICKS_2 }, // 0x05 - Decrement register B
 };
