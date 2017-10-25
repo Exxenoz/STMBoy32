e8d708d291dea9051f065c580c9882da875fa1b2
Andreas Krischke <exxenoz@gmx.at>
2017-10-25 21:39:13 +0200

    Implemented MBC emulation framework and MBC1 support

--- Project/GBC/gbc_mmu.c
+++ Project/GBC/gbc_mmu.c
@@ -1,13 +1,33 @@
 #include "gbc_mmu.h"
+#include "gbc.h"
 #include "sdc.h"
 #include "ff.h"
+#include "cmod.h"
 
-// Global GBC Memory
+// GBC Memory
 GBC_MMU_Memory_t GBC_MMU_Memory;
-// Global SDC ROM file object
+// SDC ROM file object
 FIL GBC_MMU_SDC_ROMFile;
-// Global SDC ROM file open state
+// SDC ROM file open state
 bool GBC_MMU_SDC_ROMFileStreamOpen = false;
+
+// Current Memory Bank Controller
+GBC_MMU_MemoryBankController_t GBC_MMU_MemoryBankController = GBC_MMU_MBC_NONE;
+// Current ROM Bank ID
+uint8_t GBC_MMU_CurrentROMBankID = 1;
+// Current ROM Bank Start Address
+uint32_t GBC_MMU_CurrentROMBankAddress = 16384;
+// ROM/RAM Mode Select
+GBC_MMU_MBC1Mode_t GBC_MMU_MBC1Mode = GBC_MMU_MBC1_MODE_ROM;
+// External RAM Enabled State
+bool GBC_MMU_ERAMEnabled = false;
+// Current ERAM Bank ID
+uint8_t GBC_MMU_CurrentERAMBankID = 0;
+
+void GBC_MMU_OnROMBank0Loaded()
+{
+    GBC_MMU_MemoryBankController = GBC_MMU_GetMemoryBankController();
+}
 
 bool GBC_MMU_LoadFromCartridge(void)
 {
@@ -32,6 +52,7 @@ bool GBC_MMU_LoadFromSDC(char* fileName)
         if (f_read(&GBC_MMU_SDC_ROMFile, GBC_MMU_Memory.CartridgeBank0, 16384, &bytesRead) == FR_OK && bytesRead == 16384)
         {
             GBC_MMU_SDC_ROMFileStreamOpen = true;
+            GBC_MMU_OnROMBank0Loaded();
             return true;
         }
 
@@ -49,6 +70,11 @@ void GBC_MMU_Unload(void)
 
         f_close(&GBC_MMU_SDC_ROMFile);
     }
+
+    // Reset ROM Bank ID to 1
+    GBC_MMU_CurrentROMBankID = 1;
+    // Reset Current ROM Bank Start Address to 16384
+    GBC_MMU_CurrentROMBankAddress = 16384;
 }
 
 uint8_t GBC_MMU_ReadByte(uint16_t address)
@@ -61,7 +87,21 @@ uint8_t GBC_MMU_ReadByte(uint16_t address)
     // Cartridge ROM bank X
     else if (address <= 0x7FFF)
     {
-        // ToDo: Implement ROM bank reading
+        uint8_t result = 0;
+
+        if (GBC_IsLoadedFromCartridge())
+        {
+            CMOD_ReadByte(address, &result);
+            while (CMOD_GetStatus() == CMOD_PROCESSING);
+            return result;
+        }
+        else
+        {
+            uint32_t bytesRead = 0;
+            f_lseek(&GBC_MMU_SDC_ROMFile, GBC_MMU_CurrentROMBankAddress + (address - 0x4000));
+            f_read(&GBC_MMU_SDC_ROMFile, &result, 1, &bytesRead);
+            return result;
+        }
     }
     // Video RAM bank X
     else if (address <= 0x9FFF)
@@ -117,17 +157,109 @@ uint16_t GBC_MMU_ReadShort(uint16_t address)
     return GBC_MMU_ReadByte(address) | (GBC_MMU_ReadByte(address + 1) << 8);
 }
 
+void GBC_MMU_MBC_None(uint16_t address, uint8_t value)
+{
+    // Do nothing
+}
+
+void GBC_MMU_MBC1_Write(uint16_t address, uint8_t value)
+{
+    // RAM Enable
+    if (address <= 0x1FFF)
+    {
+        GBC_MMU_ERAMEnabled = value & 0xA ? true : false;
+    }
+    // ROM Bank Number
+    else if (address <= 0x3FFF)
+    {
+        GBC_MMU_CurrentROMBankID &= ~0x1F;        // Set first 5 bits to 0
+        GBC_MMU_CurrentROMBankID |= value & 0x1F; // Set first 5 bits to value
+
+        // Any attempt to address ROM Banks 00h, 20h, 40h, 60h will select Bank 21h, 41h, and 61h instead
+        switch (GBC_MMU_CurrentROMBankID)
+        {
+            case 0x00:
+            case 0x20:
+            case 0x40:
+            case 0x60:
+                GBC_MMU_CurrentROMBankID++;
+                break;
+        }
+
+        GBC_MMU_CurrentROMBankAddress = 16384 << (GBC_MMU_CurrentROMBankID - 1);
+    }
+    // RAM Bank Number - or - Upper Bits of ROM Bank Number
+    else if (address <= 0x5FFF)
+    {
+        switch (GBC_MMU_MBC1Mode)
+        {
+            case GBC_MMU_MBC1_MODE_ROM: // Specify the upper two bits (Bit 6-7) of the ROM Bank number
+                GBC_MMU_CurrentROMBankID &= ~0x60;        // Set bit 6 and 7 to 0
+                GBC_MMU_CurrentROMBankID |= value & 0x60; // Set bit 6 and 7 to value
+
+                // Any attempt to address ROM Banks 00h, 20h, 40h, 60h will select Bank 21h, 41h, and 61h instead
+                switch (GBC_MMU_CurrentROMBankID)
+                {
+                    case 0x00:
+                    case 0x20:
+                    case 0x40:
+                    case 0x60:
+                        GBC_MMU_CurrentROMBankID++;
+                        break;
+                }
+
+                GBC_MMU_CurrentROMBankAddress = 16384 << (GBC_MMU_CurrentROMBankID - 1);
+
+                break;
+            case GBC_MMU_MBC1_MODE_RAM: // Select a RAM Bank in range from 00-03h
+                GBC_MMU_CurrentERAMBankID = value;
+                break;
+        }
+    }
+    // ROM/RAM Mode Select
+    else // address <= 0x7FFF
+    {
+        GBC_MMU_MBC1Mode = (value == 0) ? GBC_MMU_MBC1_MODE_ROM : GBC_MMU_MBC1_MODE_RAM;
+    }
+}
+
+void GBC_MMU_MBC2_Write(uint16_t address, uint8_t value)
+{
+    
+}
+
+void GBC_MMU_MBC3_Write(uint16_t address, uint8_t value)
+{
+    
+}
+
+void GBC_MMU_MBC5_Write(uint16_t address, uint8_t value)
+{
+    
+}
+
+void (*GBC_MMU_MBC_Table[5])(uint16_t, uint8_t) =
+{
+    GBC_MMU_MBC_None,
+    GBC_MMU_MBC1_Write,
+    GBC_MMU_MBC2_Write,
+    GBC_MMU_MBC3_Write,
+    GBC_MMU_MBC5_Write,
+};
+
 void GBC_MMU_WriteByte(uint16_t address, uint8_t value)
 {
-    // Cartridge ROM bank 0
-    if (address <= 0x3FFF)
+    // Memory Bank Switch
+    if (address <= 0x7FFF)
     {
-        GBC_MMU_Memory.CartridgeBank0[address] = value;
-    }
-    // Cartridge ROM bank X
-    else if (address <= 0x7FFF)
-    {
-        // ToDo: Implement ROM bank writing
+        if (GBC_IsLoadedFromCartridge())
+        {
+            CMOD_WriteByte(address, &value);
+        }
+        else // Loaded from SDC
+        {
+            GBC_MMU_MBC_Table[GBC_MMU_MemoryBankController](address, value);
+        }
     }
     // Video RAM bank X
     else if (address <= 0x9FFF)
@@ -180,4 +312,37 @@ void GBC_MMU_WriteShort(uint16_t address, uint16_t value)
 {
     GBC_MMU_WriteByte(address, value & 0xFF);
     GBC_MMU_WriteByte(address + 1, (value & 0xFF00) >> 8);
+}
+
+GBC_MMU_MemoryBankController_t GBC_MMU_GetMemoryBankController(void)
+{
+    switch (GBC_MMU_Memory.CartridgeType)
+    {
+        case GBC_MMU_CARTRIDGE_TYPE_ROM_ONLY:
+        case GBC_MMU_CARTRIDGE_TYPE_ROM_RAM:
+        case GBC_MMU_CARTRIDGE_TYPE_ROM_RAM_BATTERY:
+            return GBC_MMU_MBC_NONE;
+        case GBC_MMU_CARTRIDGE_TYPE_MBC1:
+        case GBC_MMU_CARTRIDGE_TYPE_MBC1_RAM:
+        case GBC_MMU_CARTRIDGE_TYPE_MBC1_RAM_BATTERY:
+            return GBC_MMU_MBC1;
+        case GBC_MMU_CARTRIDGE_TYPE_MBC2:
+        case GBC_MMU_CARTRIDGE_TYPE_MBC2_BATTERY:
+            return GBC_MMU_MBC2;
+        case GBC_MMU_CARTRIDGE_TYPE_MBC3_TIMER_BATTERY:
+        case GBC_MMU_CARTRIDGE_TYPE_MBC3_TIMER_RAM_BATTERY:
+        case GBC_MMU_CARTRIDGE_TYPE_MBC3:
+        case GBC_MMU_CARTRIDGE_TYPE_MBC3_RAM:
+        case GBC_MMU_CARTRIDGE_TYPE_MBC3_RAM_BATTERY:
+            return GBC_MMU_MBC3;
+        case GBC_MMU_CARTRIDGE_TYPE_MBC5:
+        case GBC_MMU_CARTRIDGE_TYPE_MBC5_RAM:
+        case GBC_MMU_CARTRIDGE_TYPE_MBC5_RAM_BATTERY:
+        case GBC_MMU_CARTRIDGE_TYPE_MBC5_RUMBLE:
+        case GBC_MMU_CARTRIDGE_TYPE_MBC5_RUMBLE_RAM:
+        case GBC_MMU_CARTRIDGE_TYPE_MBC5_RUMBLE_RAM_BATTERY:
+            return GBC_MMU_MBC5;
+    }
+
+    return GBC_MMU_MBC_NONE;
 }
--- Project/GBC/gbc_mmu.h
+++ Project/GBC/gbc_mmu.h
@@ -3,6 +3,56 @@
 
 #include "common.h"
 
+typedef enum GBC_MMU_CartridgeType_e
+{
+    GBC_MMU_CARTRIDGE_TYPE_ROM_ONLY                       = 0x00,
+    GBC_MMU_CARTRIDGE_TYPE_MBC1                           = 0x01,
+    GBC_MMU_CARTRIDGE_TYPE_MBC1_RAM                       = 0x02,
+    GBC_MMU_CARTRIDGE_TYPE_MBC1_RAM_BATTERY               = 0x03,
+    GBC_MMU_CARTRIDGE_TYPE_MBC2                           = 0x05,
+    GBC_MMU_CARTRIDGE_TYPE_MBC2_BATTERY                   = 0x06,
+    GBC_MMU_CARTRIDGE_TYPE_ROM_RAM                        = 0x08,
+    GBC_MMU_CARTRIDGE_TYPE_ROM_RAM_BATTERY                = 0x09,
+    GBC_MMU_CARTRIDGE_TYPE_MMM01                          = 0x0B,
+    GBC_MMU_CARTRIDGE_TYPE_MMM01_RAM                      = 0x0C,
+    GBC_MMU_CARTRIDGE_TYPE_MMM01_RAM_BATTERY              = 0x0D,
+    GBC_MMU_CARTRIDGE_TYPE_MBC3_TIMER_BATTERY             = 0x0F,
+    GBC_MMU_CARTRIDGE_TYPE_MBC3_TIMER_RAM_BATTERY         = 0x10,
+    GBC_MMU_CARTRIDGE_TYPE_MBC3                           = 0x11,
+    GBC_MMU_CARTRIDGE_TYPE_MBC3_RAM                       = 0x12,
+    GBC_MMU_CARTRIDGE_TYPE_MBC3_RAM_BATTERY               = 0x13,
+    GBC_MMU_CARTRIDGE_TYPE_MBC5                           = 0x19,
+    GBC_MMU_CARTRIDGE_TYPE_MBC5_RAM                       = 0x1A,
+    GBC_MMU_CARTRIDGE_TYPE_MBC5_RAM_BATTERY               = 0x1B,
+    GBC_MMU_CARTRIDGE_TYPE_MBC5_RUMBLE                    = 0x1C,
+    GBC_MMU_CARTRIDGE_TYPE_MBC5_RUMBLE_RAM                = 0x1D,
+    GBC_MMU_CARTRIDGE_TYPE_MBC5_RUMBLE_RAM_BATTERY        = 0x1E,
+    GBC_MMU_CARTRIDGE_TYPE_MBC6                           = 0x20,
+    GBC_MMU_CARTRIDGE_TYPE_MBC7_SENSOR_RUMBLE_RAM_BATTERY = 0x22,
+    GBC_MMU_CARTRIDGE_TYPE_POCKET_CAMERA                  = 0xFC,
+    GBC_MMU_CARTRIDGE_TYPE_BANDAI_TAMA5                   = 0xFD,
+    GBC_MMU_CARTRIDGE_TYPE_HuC3                           = 0xFE,
+    GBC_MMU_CARTRIDGE_TYPE_HuC1_RAM_BATTERY               = 0xFF,
+}
+GBC_MMU_CartridgeType_t;
+
+typedef enum GBC_MMU_MemoryBankController_e
+{
+    GBC_MMU_MBC_NONE = 0,
+    GBC_MMU_MBC1     = 1,
+    GBC_MMU_MBC2     = 2,
+    GBC_MMU_MBC3     = 3,
+    GBC_MMU_MBC5     = 4,
+}
+GBC_MMU_MemoryBankController_t;
+
+typedef enum GBC_MMU_MBC1Mode_e
+{
+    GBC_MMU_MBC1_MODE_ROM = 0, // Up to  8KByte RAM, 2MByte ROM
+    GBC_MMU_MBC1_MODE_RAM = 1, // Up to 32KByte RAM, 512KByte ROM
+}
+GBC_MMU_MBC1Mode_t;
+
 enum GBC_MMU_InterruptFlags_e
 {
     GBC_MMU_INTERRUPT_FLAGS_NONE     =  0,
@@ -26,42 +76,66 @@ enum GBC_MMU_MemoryLocations_e
 
 typedef struct GBC_MMU_Memory_s
 {
-    uint8_t CartridgeBank0[16384];   // 0000-3FFF: 16kB Cartridge ROM bank 0
-    //------CartridgeBankX              4000-7FFF: 16kB Cartridge ROM bank X
-    uint8_t VRAMBank0[8192];         // 8000-9FFF:  8kB Video RAM bank X                 - Switchable only in GBC mode (0-1)
+    union
+    {
+        uint8_t CartridgeBank0[16384];    // 0000-3FFF: 16kB Cartridge ROM bank 0
+
+        struct
+        {
+            uint8_t Bootstrap[256];       // 0000-00FF
+            uint8_t EntryPoint[4];        // 0100-0103
+            uint8_t NintendoLogo[48];     // 0104-0133
+            uint8_t Title[11];            // 0134-013E
+            uint8_t ManufacturerCode[4];  // 013F-0142
+            uint8_t CGBFlag;              // 0143
+            uint8_t NewLicenseeCode[2];   // 0144-0145
+            uint8_t SGBFlag;              // 0146
+            uint8_t CartridgeType;        // 0147
+            uint8_t ROMSize;              // 0148
+            uint8_t RAMSize;              // 0149
+            uint8_t DestinationCode;      // 014A
+            uint8_t OldLicenseeCode;      // 014B
+            uint8_t MaskROMVersionNumber; // 014C
+            uint8_t HeaderChecksum;       // 014D
+            uint8_t GlobalChecksum[2];    // 014E-014F
+            // Program Code               // 0150-3FFF
+        };
+    };
+    //------CartridgeBankX                   4000-7FFF: 16kB Cartridge ROM bank X
+    uint8_t VRAMBank0[8192];              // 8000-9FFF:  8kB Video RAM bank X                 - Switchable only in GBC mode (0-1)
     uint8_t VRAMBank1[8192];
-    uint8_t ERAMBank0[8192];         // A000-BFFF:  8kB External Cartridge RAM bank X
+    uint8_t ERAMBank0[8192];              // A000-BFFF:  8kB External Cartridge RAM bank X
     uint8_t ERAMBank1[8192];
     uint8_t ERAMBank2[8192];
     uint8_t ERAMBank3[8192];
-    uint8_t WRAMBank0[4096];         // C000-CFFF:  4kB Work RAM Bank 0
-    uint8_t WRAMBank1[4096];         // D000-DFFF:  4kB Work RAM Bank X                  - Switchable only in GBC mode (1-7)
+    uint8_t WRAMBank0[4096];              // C000-CFFF:  4kB Work RAM Bank 0
+    uint8_t WRAMBank1[4096];              // D000-DFFF:  4kB Work RAM Bank X                  - Switchable only in GBC mode (1-7)
     uint8_t WRAMBank2[4096];
     uint8_t WRAMBank3[4096];
     uint8_t WRAMBank4[4096];
     uint8_t WRAMBank5[4096];
     uint8_t WRAMBank6[4096];
     uint8_t WRAMBank7[4096];
-    //------ShadowRAM                // E000-FDFF: 7.5kB Shadow RAM                      - Unused due to the original GBC wiring
-    uint8_t OAM[160];                // FE00-FE9F: 160B Object Attribute Memory
-    //------Unused                   // FEA0-FEFF:  96B Unused
+    //------ShadowRAM                     // E000-FDFF: 7.5kB Shadow RAM                      - Unused due to the original GBC wiring
+    uint8_t OAM[160];                     // FE00-FE9F: 160B Object Attribute Memory
+    //------Unused                        // FEA0-FEFF:  96B Unused
     union
     {
-        uint8_t IO[128];             // FF00-FF7F: 128B Memory-mapped I/O
+        uint8_t IO[128];                  // FF00-FF7F: 128B Memory-mapped I/O
 
         struct
         {
             uint8_t IO_Unk1[15];
-            uint8_t InterruptFlags;  // 0xFF0F
+            uint8_t InterruptFlags;       // 0xFF0F
             uint8_t IO_Unk2[48];
-            uint8_t GPUControlFlags; // 0xFF40
+            uint8_t GPUControlFlags;      // 0xFF40
             uint8_t IO_Unk3;
-            uint8_t GPUScrollY;      // 0xFF42
-            uint8_t GPUScrollX;      // 0xFF43
+            uint8_t GPUScrollY;           // 0xFF42
+            uint8_t GPUScrollX;           // 0xFF43
         };
     };
-    uint8_t HRAM[127];               // FF80-FFFE: 127B High RAM
-    uint8_t InterruptEnableRegister; // FFFF:        1B Interrupt enable register
+    uint8_t HRAM[127];                    // FF80-FFFE: 127B High RAM
+    uint8_t InterruptEnableRegister;      // FFFF:        1B Interrupt enable register
 }
 GBC_MMU_Memory_t;
 
@@ -74,5 +148,7 @@ uint16_t GBC_MMU_ReadShort(uint16_t address);
 
 void GBC_MMU_WriteByte(uint16_t address, uint8_t value);
 void GBC_MMU_WriteShort(uint16_t address, uint16_t value);
+
+GBC_MMU_MemoryBankController_t GBC_MMU_GetMemoryBankController(void);
 
 #endif
