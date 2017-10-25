eee4138b8c8c90ad4034f903877959e179ebe035
Andreas Krischke <exxenoz@gmx.at>
2017-10-25 17:45:35 +0200

    Refactored GBC MMU

--- Project/GBC/gbc_mmu.c
+++ Project/GBC/gbc_mmu.c
@@ -28,7 +28,8 @@ bool GBC_MMU_LoadFromSDC(char* fileName)
     {
         uint32_t bytesRead = 0;
 
-        if (f_read(&GBC_MMU_SDC_ROMFile, GBC_MMU_Memory.Data, 32768, &bytesRead) == FR_OK && bytesRead == 32768)
+        // Read ROM Bank 0
+        if (f_read(&GBC_MMU_SDC_ROMFile, GBC_MMU_Memory.CartridgeBank0, 16384, &bytesRead) == FR_OK && bytesRead == 16384)
         {
             GBC_MMU_SDC_ROMFileStreamOpen = true;
             return true;
@@ -52,13 +53,63 @@ void GBC_MMU_Unload(void)
 
 uint8_t GBC_MMU_ReadByte(uint16_t address)
 {
-    // Shadow RAM redirection to WRAM
-    if (address >= 0xE000 && address <= 0xFDFF)
+    // Cartridge ROM bank 0
+    if (address <= 0x3FFF)
     {
-        return GBC_MMU_Memory.WRAMBank0[address - 0xE000];
+        return GBC_MMU_Memory.CartridgeBank0[address];
+    }
+    // Cartridge ROM bank X
+    else if (address <= 0x7FFF)
+    {
+        // ToDo: Implement ROM bank reading
+    }
+    // Video RAM bank X
+    else if (address <= 0x9FFF)
+    {
+        // ToDo: Implement VRAM bank switching
+    }
+    // External RAM bank X
+    else if (address <= 0xBFFF)
+    {
+        // ToDo: Implement External RAM bank reading
+    }
+    // Work RAM Bank 0
+    else if (address <= 0xCFFF)
+    {
+        return GBC_MMU_Memory.WRAMBank0[address - 0xC000];
+    }
+    // Work RAM Bank X
+    else if (address <= 0xDFFF)
+    {
+        // ToDo: Implement WRAM bank switching
+    }
+    // Shadow RAM redirection to WRAM
+    else if (address <= 0xFDFF)
+    {
+        return GBC_MMU_ReadByte(0xC000 + (address - 0xE000));
+    }
+    // Object Attribute Memory
+    else if (address <= 0xFE9F)
+    {
+        return GBC_MMU_Memory.OAM[address - 0xFE00];
+    }
+    // Unused
+    else if (address <= 0xFEFF)
+    {
+        // Do nothing
+    }
+    // Memory-mapped I/O
+    else if (address <= 0xFF7F)
+    {
+        return GBC_MMU_Memory.IO[address - 0xFF00];
+    }
+    // High RAM and Interrupt Enable Register
+    else if (address <= 0xFFFF)
+    {
+        return GBC_MMU_Memory.HRAM[address - 0xFF80];
     }
 
-    return GBC_MMU_Memory.Data[address];
+    return 0;
 }
 
 uint16_t GBC_MMU_ReadShort(uint16_t address)
@@ -68,13 +119,61 @@ uint16_t GBC_MMU_ReadShort(uint16_t address)
 
 void GBC_MMU_WriteByte(uint16_t address, uint8_t value)
 {
-    // Shadow RAM redirection to WRAM
-    if (address >= 0xE000 && address <= 0xFDFF)
+    // Cartridge ROM bank 0
+    if (address <= 0x3FFF)
     {
-        GBC_MMU_Memory.WRAMBank0[address - 0xE000] = value;
+        GBC_MMU_Memory.CartridgeBank0[address] = value;
     }
-
-    GBC_MMU_Memory.Data[address] = value;
+    // Cartridge ROM bank X
+    else if (address <= 0x7FFF)
+    {
+        // ToDo: Implement ROM bank writing
+    }
+    // Video RAM bank X
+    else if (address <= 0x9FFF)
+    {
+        // ToDo: Implement VRAM bank switching
+    }
+    // External RAM bank X
+    else if (address <= 0xBFFF)
+    {
+        // ToDo: Implement External RAM bank writing
+    }
+    // Work RAM Bank 0
+    else if (address <= 0xCFFF)
+    {
+        GBC_MMU_Memory.WRAMBank0[address - 0xC000] = value;
+    }
+    // Work RAM Bank X
+    else if (address <= 0xDFFF)
+    {
+        // ToDo: Implement WRAM bank switching
+    }
+    // Shadow RAM redirection to WRAM
+    else if (address <= 0xFDFF)
+    {
+        GBC_MMU_WriteByte(0xC000 + (address - 0xE000), value);
+    }
+    // Object Attribute Memory
+    else if (address <= 0xFE9F)
+    {
+        GBC_MMU_Memory.OAM[address - 0xFE00] = value;
+    }
+    // Unused
+    else if (address <= 0xFEFF)
+    {
+        // Do nothing
+    }
+    // Memory-mapped I/O
+    else if (address <= 0xFF7F)
+    {
+        GBC_MMU_Memory.IO[address - 0xFF00] = value;
+    }
+    // High RAM and Interrupt Enable Register
+    else if (address <= 0xFFFF)
+    {
+        GBC_MMU_Memory.HRAM[address - 0xFF80] = value;
+    }
 }
 
 void GBC_MMU_WriteShort(uint16_t address, uint16_t value)
--- Project/GBC/gbc_mmu.h
+++ Project/GBC/gbc_mmu.h
@@ -26,37 +26,42 @@ enum GBC_MMU_MemoryLocations_e
 
 typedef struct GBC_MMU_Memory_s
 {
+    uint8_t CartridgeBank0[16384];   // 0000-3FFF: 16kB Cartridge ROM bank 0
+    //------CartridgeBankX              4000-7FFF: 16kB Cartridge ROM bank X
+    uint8_t VRAMBank0[8192];         // 8000-9FFF:  8kB Video RAM bank X                 - Switchable only in GBC mode (0-1)
+    uint8_t VRAMBank1[8192];
+    uint8_t ERAMBank0[8192];         // A000-BFFF:  8kB External Cartridge RAM bank X
+    uint8_t ERAMBank1[8192];
+    uint8_t ERAMBank2[8192];
+    uint8_t ERAMBank3[8192];
+    uint8_t WRAMBank0[4096];         // C000-CFFF:  4kB Work RAM Bank 0
+    uint8_t WRAMBank1[4096];         // D000-DFFF:  4kB Work RAM Bank X                  - Switchable only in GBC mode (1-7)
+    uint8_t WRAMBank2[4096];
+    uint8_t WRAMBank3[4096];
+    uint8_t WRAMBank4[4096];
+    uint8_t WRAMBank5[4096];
+    uint8_t WRAMBank6[4096];
+    uint8_t WRAMBank7[4096];
+    //------ShadowRAM                // E000-FDFF: 7.5kB Shadow RAM                      - Unused due to the original GBC wiring
+    uint8_t OAM[160];                // FE00-FE9F: 160B Object Attribute Memory
+    //------Unused                   // FEA0-FEFF:  96B Unused
     union
     {
-        struct
-        {
-            uint8_t CartridgeBank0[16384];   // 0000-3FFF: 16kB Cartridge ROM bank 0
-            uint8_t CartridgeBankX[16384];   // 4000-7FFF: 16kB Cartridge ROM bank X
-            uint8_t VRAMBankX[8192];         // 8000-9FFF:  8kB Video RAM bank X                 - Switchable only in GBC mode (0-1)
-            uint8_t ERAMBankX[8192];         // A000-BFFF:  8kB External Cartridge RAM bank X
-            uint8_t WRAMBank0[4096];         // C000-CFFF:  4kB Work RAM Bank 0
-            uint8_t WRAMBankX[4096];         // D000-DFFF:  4kB Work RAM Bank X                  - Switchable only in GBC mode (1-7)
-            uint8_t ShadowRAM[7680];         // E000-FDFF: ~7kB Shadow RAM                       - Unused due to the original GBC wiring
-            uint8_t OAM[160];                // FE00-FE9F: 160B Object Attribute Memory
-            uint8_t Unused[96];              // FEA0-FEFF:  96B Unused
-            uint8_t IO[128];                 // FF00-FF7F: 128B Memory-mapped I/O
-            uint8_t HRAM[127];               // FF80-FFFE: 127B High RAM
-            uint8_t InterruptEnableRegister; // FFFF:        1B Interrupt enable register
-        };
+        uint8_t IO[128];             // FF00-FF7F: 128B Memory-mapped I/O
 
         struct
         {
-            uint8_t NoName1[65295];
-            uint8_t InterruptFlags;          // 0xFF0F
-            uint8_t NoName2[48];
-            uint8_t GPUControlFlags;         // 0xFF40
-            uint8_t NoName3;
-            uint8_t GPUScrollY;              // 0xFF42
-            uint8_t GPUScrollX;              // 0xFF43
+            uint8_t IO_Unk1[15];
+            uint8_t InterruptFlags;  // 0xFF0F
+            uint8_t IO_Unk2[48];
+            uint8_t GPUControlFlags; // 0xFF40
+            uint8_t IO_Unk3;
+            uint8_t GPUScrollY;      // 0xFF42
+            uint8_t GPUScrollX;      // 0xFF43
         };
-
-        uint8_t Data[65536];                 // 65kB GBC Memory
     };
+    uint8_t HRAM[127];               // FF80-FFFE: 127B High RAM
+    uint8_t InterruptEnableRegister; // FFFF:        1B Interrupt enable register
 }
 GBC_MMU_Memory_t;
 
