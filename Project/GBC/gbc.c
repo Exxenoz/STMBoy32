160316d6fe73ed804d2502886500fdaecd0988c3
Andreas Krischke <exxenoz@gmx.at>
2017-10-26 01:07:21 +0200

    Implemented GBC_LoadFromCartridge

--- Project/GBC/gbc.c
+++ Project/GBC/gbc.c
@@ -1,13 +1,26 @@
 #include "gbc.h"
 #include "gbc_mmu.h"
+#include "cmod.h"
 #include "sdc.h"
 
-// Global GBC load state
-GBC_LoadState_t GBC_LoadState = GBC_LOAD_STATE_NONE;
+GBC_LoadState_t GBC_LoadState = GBC_LOAD_STATE_NONE; // Global GBC load state
 
 GBC_LoadResult_t GBC_LoadFromCartridge(void)
 {
-    // ToDo
+    GBC_Unload();
+
+    if (!CMOD_Detect())
+    {
+        return GBC_LOAD_RESULT_NO_CARTRIDGE;
+    }
+
+    if (!GBC_MMU_LoadFromCartridge())
+    {
+        return GBC_LOAD_RESULT_NO_VALID_CARTRIDGE;
+    }
+
+    GBC_LoadState = GBC_LOAD_STATE_CARTRIDGE;
+
     return GBC_LOAD_RESULT_OK;
 }
 
@@ -33,6 +46,8 @@ GBC_LoadResult_t GBC_LoadFromSDC(char* fileName)
 void GBC_Unload(void)
 {
     GBC_MMU_Unload();
+
+    GBC_LoadState = GBC_LOAD_STATE_NONE;
 }
 
 bool GBC_IsLoaded(void)
