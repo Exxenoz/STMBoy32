c69b78d938d4eae5c7da063cafa13ab324265324
Tobias <tobi.dv@gmail.com>
2017-11-21 17:08:14 +0100

    Implemented drawing of mirrored fonts and Fonts_GetStringLength() (again)

--- Project/Fonts/myFonts.c
+++ Project/Fonts/myFonts.c
@@ -6,11 +6,26 @@ Fonts_FontDef_t Fonts_STMFont_12x12;
 
 void Fonts_InitializeSTMFonts(void)
 {
+    Fonts_STMFont_16x24.LettersMirrored = true;
     Fonts_STMFont_16x24.FontHeight = Font16x24.Height;
     Fonts_STMFont_16x24.FontWidth = Font16x24.Width;
-    Fonts_STMFont_16x24.fontData = Font16x24.table;
-    
+    Fonts_STMFont_16x24.FontData = Font16x24.table;
+
+    Fonts_STMFont_12x12.LettersMirrored = true;
     Fonts_STMFont_12x12.FontHeight = Font12x12.Height;
     Fonts_STMFont_12x12.FontWidth = Font12x12.Width;
-    Fonts_STMFont_12x12.fontData = Font12x12.table;
+    Fonts_STMFont_12x12.FontData = Font12x12.table;
+}
+
+// Returns the length of a given text represented by a certain font (in pixel)
+int  Fonts_GetStringLength(char *text, uint16_t textSpacing, Fonts_FontDef_t *font)
+{
+    int length = 0;
+
+    for (; *text != '\0'; text++)
+    {
+        length += font->FontWidth + textSpacing;
+    }
+
+    return length;
 }
