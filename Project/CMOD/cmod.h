deabe4b8814c3b41e3998c9cd3ab610cb69fde7a
Exxenoz <exxenoz@gmx.at>
2017-11-23 17:41:15 +0100

    Revert CMOD logic inversion

--- Project/CMOD/cmod.h
+++ Project/CMOD/cmod.h
@@ -6,21 +6,21 @@
 #include "led.h"
 #include "gbc_mmu.h"
 
-#define CMOD_RST_RESET       CMOD_RESET_PORT->BSRRL |= CMOD_RESET_PIN
-#define CMOD_SET_RESET       CMOD_RESET_PORT->BSRRH |= CMOD_RESET_PIN
+#define CMOD_SET_RESET       CMOD_RESET_PORT->BSRRL |= CMOD_RESET_PIN
+#define CMOD_RST_RESET       CMOD_RESET_PORT->BSRRH |= CMOD_RESET_PIN
                              
-#define CMOD_RST_CS          CMOD_CS_PORT->BSRRL |= CMOD_CS_PIN
-#define CMOD_SET_CS          CMOD_CS_PORT->BSRRH |= CMOD_CS_PIN
+#define CMOD_SET_CS          CMOD_CS_PORT->BSRRL |= CMOD_CS_PIN
+#define CMOD_RST_CS          CMOD_CS_PORT->BSRRH |= CMOD_CS_PIN
                              
-#define CMOD_RST_RD          CMOD_RD_PORT->BSRRL |= CMOD_RD_PIN
-#define CMOD_SET_RD          CMOD_RD_PORT->BSRRH |= CMOD_RD_PIN
+#define CMOD_SET_RD          CMOD_RD_PORT->BSRRL |= CMOD_RD_PIN
+#define CMOD_RST_RD          CMOD_RD_PORT->BSRRH |= CMOD_RD_PIN
                              
-#define CMOD_RST_WR          CMOD_WR_PORT->BSRRL |= CMOD_WR_PIN
-#define CMOD_SET_WR          CMOD_WR_PORT->BSRRH |= CMOD_WR_PIN
+#define CMOD_SET_WR          CMOD_WR_PORT->BSRRL |= CMOD_WR_PIN
+#define CMOD_RST_WR          CMOD_WR_PORT->BSRRH |= CMOD_WR_PIN
                              
-#define CMOD_SET_ADDR(ADDR)  CMOD_ADDR_PORT->ODR = (~ADDR)
-#define CMOD_SET_DATA(DATA)  CMOD_DATA_PORT->ODR = (~DATA)
-#define CMOD_GET_DATA()      ((~(CMOD_DATA_PORT->IDR)) & 0xFF)
+#define CMOD_SET_ADDR(ADDR)  CMOD_ADDR_PORT->ODR = (ADDR)
+#define CMOD_SET_DATA(DATA)  CMOD_DATA_PORT->ODR = (DATA)
+#define CMOD_GET_DATA()      (((CMOD_DATA_PORT->IDR)) & 0xFF)
                              
 #define CMOD_DATA_MODE_IN    GPIOG->MODER = 0x00000000
 #define CMOD_DATA_MODE_OUT   GPIOG->MODER = 0x00005555
