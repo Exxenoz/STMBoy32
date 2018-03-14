105f5dd2a0405adb787850becdc462881e3469e6
Exxenoz <exxenoz@gmx.at>
2018-03-14 18:23:24 +0100

    Updated audio module to HAL usage

--- Project/Audio/audio.c
+++ Project/Audio/audio.c
@@ -1,7 +1,8 @@
 #include "audio.h"
 
 TIM_HandleTypeDef Audio_TimerHandle = { .Instance = AUDIO_TIM };
-DMA_HandleTypeDef DMA_Handle;
+DAC_ChannelConfTypeDef Audio_ChannelConfig;
+DAC_HandleTypeDef Audio_DACHandle = { .Instance = AUDIO_DMA };
 
 void Audio_InitializeGPIO(void)
 {
@@ -15,62 +16,37 @@ void Audio_InitializeGPIO(void)
 
 void Audio_InitializeTimer(void)
 {
-    Audio_TimerHandle.Init.Period        = 5626; // 32 kHz
+    Audio_TimerHandle.Init.Period        = 3124; // 32 kHz
     Audio_TimerHandle.Init.Prescaler     = 0;
     Audio_TimerHandle.Init.CounterMode   = TIM_COUNTERMODE_UP;
     Audio_TimerHandle.Init.ClockDivision = 0;
-
-    //TIM_SelectOutputTrigger(AUDIO_TIM, TIM_TRGOSource_Update);
-
     HAL_TIM_Base_Init(&Audio_TimerHandle);
+
+    TIM_MasterConfigTypeDef masterConfig;
+    masterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+    masterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+    HAL_TIMEx_MasterConfigSynchronization(&Audio_TimerHandle, &masterConfig);
+
     HAL_TIM_Base_Start(&Audio_TimerHandle);
-}
-
-void Audio_InitializeDAC(void)
-{
-    __HAL_RCC_DAC12_CLK_ENABLE();
-
-    //DAC_InitTypeDef DAC_InitObject = {0};
-    //DAC_InitObject.DAC_Trigger = AUDIO_DAC_TRIGGER;
-    //DAC_InitObject.DAC_WaveGeneration = DAC_WaveGeneration_None;
-    //DAC_InitObject.DAC_OutputBuffer = DAC_OutputBuffer_Enable;
-    //DAC_Init(AUDIO_DAC_CHANNEL, &DAC_InitObject);
-    //
-    //DAC_Cmd(AUDIO_DAC_CHANNEL, ENABLE);
-    //
-    //DAC_DMACmd(AUDIO_DAC_CHANNEL, ENABLE);
 }
 
 void Audio_Initialize(void)
 {
+    __HAL_RCC_DAC12_CLK_ENABLE();
+    __HAL_RCC_DMA1_CLK_ENABLE();
+
     Audio_InitializeGPIO();
     Audio_InitializeTimer();
-    Audio_InitializeDAC();
 }
 
 void Audio_SetAudioBuffer(uint16_t* audioBuffer, uint32_t audioBufferSize)
 {
-    __HAL_RCC_DMA1_CLK_ENABLE();
+    HAL_DAC_DeInit(&Audio_DACHandle);
+    HAL_DAC_Init(&Audio_DACHandle);
 
-    //DMA_DeInit(AUDIO_DMA_STREAM);
-    //
-    //DMA_InitTypeDef DMA_InitObject = {0};
-    //DMA_InitObject.DMA_Channel = AUDIO_DMA_CHANNEL;  
-    //DMA_InitObject.DMA_PeripheralBaseAddr = (uint32_t)DAC_DHR12R1_ADDRESS;
-    //DMA_InitObject.DMA_Memory0BaseAddr = (uint32_t)audioBuffer;
-    //DMA_InitObject.DMA_DIR = DMA_DIR_MemoryToPeripheral;
-    //DMA_InitObject.DMA_BufferSize = audioBufferSize;
-    //DMA_InitObject.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-    //DMA_InitObject.DMA_MemoryInc = DMA_MemoryInc_Enable;
-    //DMA_InitObject.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-    //DMA_InitObject.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
-    //DMA_InitObject.DMA_Mode = DMA_Mode_Circular;
-    //DMA_InitObject.DMA_Priority = DMA_Priority_High;
-    //DMA_InitObject.DMA_FIFOMode = DMA_FIFOMode_Disable;
-    //DMA_InitObject.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
-    //DMA_InitObject.DMA_MemoryBurst = DMA_MemoryBurst_Single;
-    //DMA_InitObject.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
-    //DMA_Init(AUDIO_DMA_STREAM, &DMA_InitObject);
-    //
-    //DMA_Cmd(AUDIO_DMA_STREAM, ENABLE);
+    Audio_ChannelConfig.DAC_Trigger = AUDIO_DAC_TRIGGER;
+    Audio_ChannelConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
+
+    HAL_DAC_ConfigChannel(&Audio_DACHandle, &Audio_ChannelConfig, AUDIO_DAC_CHANNEL);
+    HAL_DAC_Start_DMA(&Audio_DACHandle, AUDIO_DAC_CHANNEL, (uint32_t *)audioBuffer, audioBufferSize, DAC_ALIGN_8B_R);
 }
--- Project/Audio/audio_config.h
+++ Project/Audio/audio_config.h
@@ -9,12 +9,12 @@
 #define AUDIO_TIM_BUS      RCC_APB2Periph_TIM8
 
 #define AUDIO_DAC_BUS      RCC_APB1Periph_DAC
-#define AUDIO_DAC_CHANNEL  DAC_Channel_1
-#define AUDIO_DAC_TRIGGER  DAC_Trigger_T8_TRGO
+#define AUDIO_DAC_CHANNEL  DAC_CHANNEL_1
+#define AUDIO_DAC_TRIGGER  DAC_TRIGGER_T8_TRGO
 
 #define AUDIO_DMA          DMA1
 #define AUDIO_DMA_BUS      RCC_AHB1Periph_DMA1
-#define AUDIO_DMA_STREAM   DMA1_Stream5
-#define AUDIO_DMA_CHANNEL  DMA_Channel_7
+#define AUDIO_DMA_STREAM   DMA1_Stream7
+#define AUDIO_DMA_CHANNEL  DMA_CHANNEL_5
 
 #endif
