3b13304681d8487bf0984d4793705a0e5688c948
Tobias <tobi.dv@gmail.com>
2018-03-14 16:47:45 +0100

    _andleObject gets initialized in HAL_SD_InitCard.

--- Libraries/ff/diskio.c
+++ Libraries/ff/diskio.c
@@ -15,10 +15,7 @@
 #define DEV_MMC		1	/* Example: Map MMC/SD card to physical drive 1 */
 #define DEV_USB		2	/* Example: Map USB MSD to physical drive 2 */
 
-SD_HandleTypeDef SD_HandleObject =
-{
-    // ToDo: Implement
-};
+SD_HandleTypeDef SD_HandleObject;
 
 /*-----------------------------------------------------------------------*/
 /* Get Drive Status                                                      */
